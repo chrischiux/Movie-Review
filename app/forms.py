54413a110adbd110b7a357807f1c3f6d685e0dcc
Chris Chiu <sc22phc@leeds.ac.uk>
2023-11-28 18:06:20 +0000

    pep8 styled

--- app/forms.py
+++ app/forms.py
@@ -5,9 +5,9 @@ from wtforms.validators import InputRequired, Length, Optional
 
 class ReviewForm(FlaskForm):
     content = TextAreaField('review',
-                        validators=[InputRequired(
-                             "review can't be empty."),
-                                   Length(min=1, max=500)])
+                            validators=[InputRequired(
+                                "review can't be empty."),
+                                Length(min=1, max=500)])
     id = HiddenField('id',
                      id='movieID',
                      default=-1,
@@ -16,10 +16,10 @@ class ReviewForm(FlaskForm):
 
 class LoginForm(FlaskForm):
     email = EmailField('email',
-                        validators=[InputRequired(
-                            "Please enter your email address."),
-                                    Length(min=1, max=64)])
+                       validators=[InputRequired(
+                           "Please enter your email address."),
+                           Length(min=1, max=64)])
     password = PasswordField('password',
-                           validators=[InputRequired(
-                               "Please enter your password."),
-                                       Length(min=1, max=64)])
+                             validators=[InputRequired(
+                                 "Please enter your password."),
+                                 Length(min=1, max=64)])
--- app/models.py
+++ app/models.py
@@ -2,16 +2,20 @@ from app import db
 from flask_login import UserMixin
 from sqlalchemy import UniqueConstraint
 
+
 class Reviews(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'))
 
-    user = db.relationship('Users', backref=db.backref('reviews', lazy='dynamic'))
-    movie = db.relationship('Movies', backref=db.backref('reviews', lazy='dynamic'))
+    user = db.relationship(
+        'Users', backref=db.backref('reviews', lazy='dynamic'))
+    movie = db.relationship(
+        'Movies', backref=db.backref('reviews', lazy='dynamic'))
 
-    __table_args__ = (UniqueConstraint('user_id', 'movie_id', name='_user_movie_uc'),)
+    __table_args__ = (UniqueConstraint(
+        'user_id', 'movie_id', name='_user_movie_uc'),)
 
 
 class Users(UserMixin, db.Model):
@@ -29,10 +33,13 @@ class Movies(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64))
     year = db.Column(db.String(64))
-    liked_users = db.relationship('Users', secondary='collection', overlaps='collection')
+    liked_users = db.relationship(
+        'Users', secondary='collection', overlaps='collection')
 
 
 collection = db.Table('collection', db.Model.metadata,
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
-    db.Column('movie_id', db.Integer, db.ForeignKey('movies.id'))
-)
+                      db.Column('user_id', db.Integer,
+                                db.ForeignKey('users.id')),
+                      db.Column('movie_id', db.Integer,
+                                db.ForeignKey('movies.id'))
+                      )
